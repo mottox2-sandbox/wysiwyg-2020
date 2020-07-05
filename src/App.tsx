d7aaec91c23cd45ca3c7c0b441860233542f8cae
mottox2 <y.mottox2@gmail.com>
2020-07-06 00:20:08 +0900

    Use react-prosemirror

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
+import Editor from './Editor'
 
 function App() {
   const [value, setValue] = useState('')
   return (
     <div className="App">
+      <Editor value={value} onChange={(value: any) => setValue(value)} />
+      {JSON.stringify(value)}
     </div>
   );
 }
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare module '@aeaton/react-prosemirror' {
+  export const HtmlEditor: any
+  export const MenuBar: any
+}
+
+declare module '@aeaton/react-prosemirror-config-default' {
+  export const options: any
+  export const menu: any
+}
